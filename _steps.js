cf37213724bba499bf9f7d1d345d36ba7cd5e370
Hossen <mdmitho199911@gmail.com>
2022-04-21 15:01:38 +0600

    send data using mongod

---
+++ _steps.js
@@ -0,0 +1,27 @@
+
+/*****
+ * 1 Create a node server with 5 steps
+ * 1.1 Create folder
+ * 1.2. npm init
+ * 1.3 npm i express cors mongodb
+ * 1.4 script : index.js
+ * 1.5 script-dev : nodemon index.js
+ * 1.6 use 5 steps to create a node server
+ * 
+ * ------------------- 
+ * Create atlas Account
+ * -------------------
+ * 
+ * 1. Sign up .google access
+ * 2. Create Cluster
+ * 3. Create user dbUser and password
+ * 4. Network Access --> ip address : allow all 
+ * 5. database > Connect > code copy paste in index.js
+ * 
+ * ---------------------
+ * CRUD Operation
+ * ---------------------
+ * 1 node mongodb CRUD > Fundamentals
+ * 2 create async run function
+ * 
+ */
---
+++ index.js
@@ -0,0 +1,43 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+//use middleware
+// app.use(cors());
+app.use(express.json())
+
+//mongodb user name : dbuser1 
+//mongodb user password : hcWPwqiCrwH2jzYd
+
+
+
+const uri = "mongodb+srv://dbuser1:hcWPwqiCrwH2jzYd@cluster0.c59qt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try{
+    await client.connect()
+    const userCollection = client.db("foodExpress").collection
+
+    app.post('/user',(req, res)=>{
+      
+    })
+  }
+  finally{
+    // await client.close()
+  }
+
+}
+
+run().catch(console.dir)
+
+
+app.get('/', (req, res)=> {
+    res.send('Running My Node CRUD Server')
+})
+
+app.listen(port,()=>{
+    console.log('CRUD Server is running');
+})

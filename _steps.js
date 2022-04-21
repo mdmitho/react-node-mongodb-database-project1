dd068f57bf37d86229391e100622c96983471ab6
Hossen <mdmitho199911@gmail.com>
2022-04-21 17:49:42 +0600

    display data from database ,create delete button

--- _steps.js
+++ _steps.js
@@ -39,5 +39,11 @@
  3 collection.find (query)
  4 cursor.toArray()
  5 return the result
- *
+ *6  from client useEffect and display like you have done before
+
+ ---------------------------
+ DELETE
+ --------------------
+
+ 
  */
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const express = require('express')
 const app = express()
 const cors= require('cors')
 const port = process.env.PORT || 5000
+const objectId = require('mongodb').ObjectId
 
 //use middleware
 app.use(cors());
@@ -21,7 +22,7 @@ async function run(){
   try{
     await client.connect()
     const userCollection = client.db("foodExpress").collection('user')
-
+// get users
     app.get('/user',async(req, res)=>{
       const query ={};
       const cursor = userCollection.find(query)
@@ -30,12 +31,18 @@ async function run(){
 
     })
 
-
+//post user : add a new user
     app.post('/user', async(req, res)=>{
       const newUser = req.body;
       console.log('adding new user', newUser);
       const result = await userCollection.insertOne(newUser)
       res.send(result)
+    })
+
+    //delete a user
+    app.delete('/user/:id', async(req, res)=>{
+ const id = req.params.id;
+ const query = {_id: ObjectId(id)}
     })
   }
   finally{
